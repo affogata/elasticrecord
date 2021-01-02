76e8a42b215f00ba31a0670c127b85fe54ff8e0f
roeesheelo <roee@commun.it>
2021-01-02 23:33:22 +0200

    version and base file

---
+++ lib/elasticrecord.rb
@@ -0,0 +1,19 @@
+
+require 'elasticrecord/version'
+require 'elasticrecord/client'
+
+require 'elasticrecord/core/queries/aggregations'
+require 'elasticrecord/core/queries/boolean'
+require 'elasticrecord/core/queries/match'
+require 'elasticrecord/core/queries/query_string'
+require 'elasticrecord/core/queries/suggestions'
+require 'elasticrecord/core/queries/operations'
+
+require 'elasticrecord/core/query'
+require 'elasticrecord/core/search'
+require 'elasticrecord/core/record'
+
+require 'elasticrecord/core/response/response'
+require 'elasticrecord/core/response/search_response'
+require 'elasticrecord/core/response/search_response_aggregation'
+require 'elasticrecord/core/response/search_response_hit'
